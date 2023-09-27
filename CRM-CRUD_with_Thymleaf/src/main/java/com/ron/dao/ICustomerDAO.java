ae4321fefcb9f6b2b170ded076863a29ee9225a1
rohan-salve <68870159+rohan-salve@users.noreply.github.com>
2023-09-27 14:37:00 +0530

    Delete CRM-CRUD_with_Thymleaf directory

--- CRM-CRUD_with_Thymleaf/src/main/java/com/ron/dao/ICustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ron.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ron.model.Customer;
-
-public interface ICustomerDAO extends CrudRepository<Customer, Integer> {
-
-}
